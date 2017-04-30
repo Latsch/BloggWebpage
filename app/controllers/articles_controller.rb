b665576c529d5751e4ff16963ec460c701c005eb
latsch <sbrossmer@googlemail.com>
2017-04-30 15:08:42 +0200

    Added workflow functionality (edit, update, delete)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,40 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
+		@articles = Article.find(params[:id])
+	end
+
+	def new
+		@articles = Article.new		
+	end
+
+	def create
+		@articles = Article.new(article_params)
+		@articles.save
+		flash.notice = "Article '#{@articles.title}' created!"
+		redirect_to article_path(@articles)
+	end
+
+	def update
+		@articles = Article.find(params[:id])
+		@articles.update(article_params)
+
+		flash.notice = "Article '#{@articles.title}' Updated!"
+		redirect_to article_path(@articles)
+	end
+
+	def destroy
+		@articles = Article.find(params[:id]).destroy
+		flash.notice = "Article '#{@articles.title}' removed!"
+		redirect_to articles_path
+	end
+
+	def edit
 		@articles = Article.find(params[:id])
 	end
 end
